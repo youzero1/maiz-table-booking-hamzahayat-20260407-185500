19e7e8078ffae0242b7bc9b693ae707e08a335b6
hamzahayatunstuck <hamza.hayat@unstucklabs.com>
2026-04-07 19:03:50 +0500

    Initial commit from Flare Codegen (API)

---
+++ next.config.mjs
@@ -0,0 +1,26 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'via.placeholder.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'graph.facebook.com',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
